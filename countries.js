a9b32684a6f069226afa618bb1a0d43fb53f4895
Steve Winberg <stevewin21@gmail.com>
2019-11-18 15:59:37 +0700

    remove iife for countries.js

--- countries.js
+++ countries.js
@@ -1,18 +1,16 @@
 'use strict';
 
-(() => {
-  // include dependencies
-  const _ = require('underscore');
-  const db = require('@scuba-squad/country-db');
+// include dependencies
+const _ = require('underscore');
+const db = require('@scuba-squad/country-db');
 
-  const countries = _.map(db, (country) => {
-    if (country.postalCodeRegEx) {
-      country.postalCodeRegEx = new RegExp(`^${country.postalCodeRegEx}$`, 'i');
-    }
+const countries = _.map(db, (country) => {
+  if (country.postalCodeRegEx) {
+    country.postalCodeRegEx = new RegExp(`^${country.postalCodeRegEx}$`, 'i');
+  }
 
-    return country;
-  });
+  return country;
+});
 
-  // export as commonjs module
-  module.exports = countries;
-})(); // end IIFE
+// export as commonjs module
+module.exports = countries;
