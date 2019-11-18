7795f0bfbc71a7072817718cb515ce533f326d19
Steve Winberg <stevewin21@gmail.com>
2019-11-18 15:58:21 +0700

    remove iife for test/countries.js

--- test/countries.js
+++ test/countries.js
@@ -1,19 +1,17 @@
 'use strict';
 
-(() => {
-  // include dependencies
-  const unit = require('unit.js');
-  const countries = require('../countries');
+// include dependencies
+const unit = require('unit.js');
+const countries = require('../countries');
 
-  // describe countries
-  describe('countries', () => {
-    it('should be an array of object literals', () => {
-      unit
-        .array(countries)
-        .hasLength(249)
-        .matchEach((v) => {
-          return !!(v.name && v.iso2Code && v.iso3Code && v.isoNumericCode);
-        });
-    }); // end it
-  }); // end describe countries
-})(); // end IIFE
+// describe countries
+describe('countries', () => {
+  it('should be an array of object literals', () => {
+    unit
+      .array(countries)
+      .hasLength(249)
+      .matchEach((v) => {
+        return !!(v.name && v.iso2Code && v.iso3Code && v.isoNumericCode);
+      });
+  }); // end it
+}); // end describe countries
