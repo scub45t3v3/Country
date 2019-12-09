3c0a70b82c1ed42498bc089068070bac8684e7e4
Steve Winberg <stevewin21@gmail.com>
2019-12-09 15:47:00 +0700

    remove underscore dependency from countries.js

--- countries.js
+++ countries.js
@@ -1,10 +1,9 @@
 'use strict';
 
 // include dependencies
-const _ = require('underscore');
 const db = require('@scuba-squad/country-db');
 
-const countries = _.map(db, (country) => {
+const countries = db.map((country) => {
   if (country.postalCodeRegEx) {
     country.postalCodeRegEx = new RegExp(`^${country.postalCodeRegEx}$`, 'i');
   }
